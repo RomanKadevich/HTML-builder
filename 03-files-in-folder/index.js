a939cf8b4eab9c784f867f8899d16121d2653360
RomanKadevich <romankadevich@gmail.com>
2023-05-08 17:28:45 +0300

    feat: complete task - 04-copy-directory

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,44 +1,43 @@
-const fs = require ('fs');
-const path = require ('path');
+const fs = require('fs');
+const path = require('path');
 const dirPath = path.join(__dirname, "secret-folder");
-// console.log(dirPath);
 
-function getFileInfo(filePath, name){
+
+function getFileInfo(filePath, name) {
     fs.stat(filePath, (err, stats) => {
         if (err) throw err;
-        if (path.extname(filePath) === ''){
-            console.log(`Имя отстутствует - ${name} - ${stats.size / 1000}kb`)
-        }else{
-        console.log(`${path.basename(filePath, path.extname(filePath))} - ${path.extname(filePath)} - ${stats.size / 1000}kb`);
-        // console.log(`File size: ${stats.size} bytes`);
-        // console.log(`Last modified: ${stats.mtime}`);
-        // console.log(`Is file: ${stats.isFile()}`);
-        // console.log(`Is directory: ${stats.isDirectory()}`);
-        // console.log(`Mode: ${stats.mode}\n\n`);
+        if (path.extname(filePath) === '') {
+            console.log(`Имя отстутствует - ${name.slice(1)} - ${stats.size / 1000}kb`)
+        } else {
+            console.log(`${path.basename(filePath, path.extname(filePath))} - ${path.extname(filePath).slice(1)} - ${stats.size / 1000}kb`);
         }
-      });
+    });
 }
 
-fs.readdir(dirPath, {withFileTypes: true}, (err, dirents) => {
+fs.readdir(dirPath, { withFileTypes: true }, (err, dirents) => {
     if (err) throw err;
-    for (let item of dirents){
-        if (!item.isDirectory()){
-           
-          getFileInfo(path.join(dirPath, item.name), item.name);
-            
+    for (let item of dirents) {
 
-    }
-        if (item.isDirectory()){
-           let dirInDirPath = path.join(__dirname, "secret-folder", item.name);
-           console.log(dirInDirPath);
-           fs.readdir(dirInDirPath, {withFileTypes: true}, (err, files) => {
-            if (err) throw err;
-            for (let file of files){
-                if (!file.isDirectory()){
-                    getFileInfo(path.join(dirPath, item.name, file.name), file.name)
-                }
-              
-            }
-        })
+        // получение информации о файлах за исключением внутренней директории при ее наличии
+        if (!item.isDirectory()) {
+
+            getFileInfo(path.join(dirPath, item.name), item.name);
+
         }
-    }})
+
+        // получение информации о файлах во внутренней директории при ее наличии
+        if (item.isDirectory()) {
+            let dirInDirPath = path.join(__dirname, "secret-folder", item.name);
+            console.log(dirInDirPath);
+            fs.readdir(dirInDirPath, { withFileTypes: true }, (err, files) => {
+                if (err) throw err;
+                for (let file of files) {
+                    if (!file.isDirectory()) {
+                        getFileInfo(path.join(dirPath, item.name, file.name), file.name)
+                    }
+
+                }
+            })
+        }
+    }
+})
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,57 @@
+const path = require('path');
+const fs = require('fs');
+const dirPath = path.join(__dirname, 'files');
+const targetDir = path.join(__dirname, 'files-copy');
+function readWriteFile(filePath) {
+    const targetPath = path.join(targetDir, path.basename(filePath));
+    const readStream = fs.createReadStream(filePath);
+    const writeStream = fs.createWriteStream(targetPath);
+    readStream.pipe(writeStream);
+    readStream.on('error', (err) => {
+        if (err) throw err;
+    })
+}
+fs.access(targetDir, (err) => {
+    if (!err) {
+        // папка существует, удаляем ее
+        fs.rm(targetDir, { recursive: true }, (err) => {
+            if (err) throw err;
+            fs.mkdir(targetDir, { recursive: true }, (err) => {
+                if (err) throw err;
+                fs.readdir(dirPath, { withFileTypes: true }, (err, dirents) => {
+                    if (err) throw err;
+                    for (let item of dirents) {
+
+                        // получение информации о файлах за исключением внутренней директории при ее наличии
+                        if (!item.isDirectory()) {
+                            let file = path.join(dirPath, item.name)
+                            readWriteFile(file);
+                        }
+                    }
+                })
+            });
+        });
+    } else {
+        // папка не существует, создаем ее
+        fs.mkdir(targetDir, { recursive: true }, (err) => {
+            if (err) throw err;
+            fs.readdir(dirPath, { withFileTypes: true }, (err, dirents) => {
+                if (err) throw err;
+                for (let item of dirents) {
+
+                    // получение информации о файлах за исключением внутренней директории при ее наличии
+                    if (!item.isDirectory()) {
+                        let file = path.join(dirPath, item.name)
+                        readWriteFile(file);
+                    }
+                }
+            })
+        });
+    }
+});
+
+
+
+
+
 
