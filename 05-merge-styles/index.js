3ca9ff0784fc9d0ce587fb4a2b062d1388679023
RomanKadevich <romankadevich@gmail.com>
2023-05-08 20:00:01 +0300

    refector: add comments in the task 5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -6,17 +6,21 @@ let readStream;
 
 fs.readdir(inputDir, { withFileTypes: true }, (err, dirents) => {
     if (err) throw err;
+
     const writeStream = fs.createWriteStream(outputFile);
+    // для каждого файла создаем поток чтения и записываем все файлы в один файл для записи
     for (let item of dirents) {
         let inputFilePath = path.join(inputDir, item.name)
+        // проверяем, что файл не директория и имеет расширение стилей
         if (!item.isDirectory() && (path.extname(inputFilePath) === '.css')) {
-         readStream = fs.createReadStream(inputFilePath);
-         readStream.pipe(writeStream, { end: false });
-         readStream.on('end', () => {
-            writeStream.write('\n');
-          });
+            readStream = fs.createReadStream(inputFilePath);
+            readStream.pipe(writeStream, { end: false });
+            //  после записи файла делаем перенос, чтобы файлы не сливались
+            readStream.on('end', () => {
+                writeStream.write('\n');
+            });
         }
-      
+
     }
 })
 
